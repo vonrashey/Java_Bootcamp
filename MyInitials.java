66ed8febcad5b4b4d1f8f5d5ef195578cc165cdf
vonrashey <vonrashey@gmail.com>
2023-04-27 21:52:00 +0200

    My Initials Java Program

---
+++ MyInitials.java
@@ -0,0 +1,13 @@
+public class MyInitials {
+    public static void main(String[] args) {
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println("Y   Y  M   M");
+         System.out.println("Y   Y  MM MM");
+         System.out.println(" Y Y   M M M");
+         System.out.println("  Y    M   M");
+         System.out.println("  Y    M   M");
+         System.out.println("  Y    M   M");
+         System.out.println("  Y    M   M");
+    }
+}
+
